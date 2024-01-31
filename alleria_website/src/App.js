155a5bc06fbaa099c58b9d10f6b561c58453717e
Christopher Wandel <cwandel2@gmail.com>
2024-01-31 15:20:43 -0500

    Test Commit

--- alleria_website/src/App.js
+++ alleria_website/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Alleria Baby Tracking
+        Alleria Baby Tracking - Testing
       </header>
     </div>
   );
