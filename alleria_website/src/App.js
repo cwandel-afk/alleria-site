8bedacc4aaec496f318bc0a1db23c175b34e9b4d
Christopher Wandel <cwandel2@gmail.com>
2024-01-31 14:08:32 -0500

    Initial set up

---
+++ alleria_website/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Alleria Baby Tracking
+      </header>
+    </div>
+  );
+}
+
+export default App;
